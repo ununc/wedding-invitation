568d8c9bee7b3c58196dd5f197e1b76ea7b56703
iamidlek <iamidlek@naver.com>
2024-07-12 10:13:07 +0900

    outside

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+const regexp = /kakaotalk/i;
+
+if (navigator.userAgent.toLowerCase().match(regexp)) {
+  location.href =
+    'kakaotalk://web/openExternal?url=' +
+    encodeURIComponent('https://wedding-yhc-lsm.netlify.app');
+} else {
+  ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+}
